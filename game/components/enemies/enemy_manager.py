34bf0c99c6948335891141cd49eea797bfd96b59
Jorge -Antonio Pérez Aruzamén <jorgeperez02022020@gmail.com>
2023-07-19 22:46:38 -0400

    Clase 3
    Adición de balas del enemigo.

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,26 +1,28 @@
-import pygame
-from game.components.enemies.enemy_1 import Enemy1
-from game.components.enemies.enemy_2 import Enemy2
+import random
+
+from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_1, ENEMY_2, ENEMY_3
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
+        self.enemy_images = [ENEMY_1, ENEMY_2, ENEMY_3]
     
-    def update(self):
+    def update(self, game):
         self.add_enemy()
 
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
-            enemy1 = Enemy1()
-            self.enemies.append(enemy1)
-            enemy2 = Enemy2()
-            self.enemies.append(enemy2)
-            
-    
+        if len(self.enemies) < 2:
+            image = random.choice(self.enemy_images)
+            speed_on_x = random.randint(10, 20)
+            speed_on_y = random.randint(1, 5)
+
+            enemy = Enemy(image, speed_on_x, speed_on_y)
+            self.enemies.append(enemy)
